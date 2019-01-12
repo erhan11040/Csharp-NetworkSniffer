499425cc536d44087a13a754b92d38b21152fae0
HeathCliff <erhan11040@gmail.com>
2019-01-12 22:17:38 +0300

    Project Uploaded

---
+++ NetworkProject/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace NetworkProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.DGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.DGV.Location = new System.Drawing.Point(12, 82);
+            this.DGV.Name = "DGV";
+            this.DGV.Size = new System.Drawing.Size(776, 356);
+            this.DGV.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(539, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ÇIKIŞ";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 59;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "VARIŞ";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 64;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PROTOKOL";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 91;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "VERİ";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 57;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(483, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Filtrele";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.DGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NetworkProject/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace NetworkProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Socket socketz = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+        byte[] bytedata = new byte[4096];
+        IPAddress myip;
+        Boolean started = false;
+        Size sizediff;
+        Boolean formloaded = false;
+        IPAddress FilterIPAddress = new IPAddress(0);
+        Boolean FilterIP;
+        System.Net.NetworkInformation.NetworkInterface[] mycomputerconnections;
+        String stringz;
+        String typez;
+        IPAddress ipfrom;
+        IPAddress ipto;
+        UInt16 destinationport;
+        UInt16 sourceport;
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sizediff.Height = this.Height - DGV.Height;
+            sizediff.Width = this.Width - DGV.Width;
+            formloaded = true;
+            mycomputerconnections = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+
+            /*
+            for (int i = 0; i < mycomputerconnections.Length - 1; i++)
+            {
+                comboBox1.Items.Add(mycomputerconnections.);
+            }
+            */
+            foreach (System.Net.NetworkInformation.NetworkInterface item in mycomputerconnections)
+            {
+                comboBox1.Items.Add(item.Name);
+            }
+
+            
+
+        }
+
+        private void OnReceive(IAsyncResult asyncResult)
+        {
+            if (started == true)
+            {
+                uint readlength = BitConverter.ToUInt16(Byteswap(bytedata, 2), 0);
+                sourceport = BitConverter.ToUInt16(Byteswap(bytedata, 22), 0);
+                destinationport = BitConverter.ToUInt16(Byteswap(bytedata, 24), 0);
+
+                if (bytedata[9] == 6)
+                {
+                    typez = "TCP";
+                }
+                else if (bytedata[9] == 17)
+                {
+                    typez = "UDP";
+                }
+                else
+                    typez = "???";
+
+                ipfrom = new IPAddress(BitConverter.ToUInt32(bytedata, 12));
+                ipto = new IPAddress(BitConverter.ToUInt32(bytedata, 16));
+
+                if (ipfrom.Equals(myip) == true || ipto.Equals(myip) == true)
+                {
+                    if (FilterIP == false || (FilterIP == true && (FilterIPAddress.Equals(ipfrom) || FilterIPAddress.Equals(ipto))))
+                    {
+                        stringz = "";
+                        for (uint i = 26; i < readlength; i++)
+                        {
+                            if (Char.IsLetterOrDigit(Convert.ToChar(bytedata[i]))==true)
+                            {
+                                stringz = stringz + Convert.ToChar(bytedata[i]);
+                            }
+                            else
+                            {
+                                stringz = stringz + "";
+                            }
+                        }
+                        DGV.Invoke(new MethodInvoker(DGVUpdate));
+
+                    }
+                }
+
+            }
+
+            socketz.BeginReceive(bytedata, 0, bytedata.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+
+
+        }
+
+        private void DGVUpdate()
+        {
+            if (DGV.Rows.Count > 50)
+            {
+                DGV.Rows.RemoveAt(0);
+            }
+
+            string[] row = new string[] { ipfrom.ToString() + ":" + sourceport, ipto.ToString() + ":" + destinationport, typez, stringz };
+
+            DGV.Rows.Add(row);
+        }
+
+        private byte[] Byteswap(byte[] bytez, uint index)
+        {
+            byte[] result = new byte[2];
+            result[0] = bytez[index + 1];
+            result[1] = bytez[index];
+            return result;
+        }
+
+        private void BindSocket()
+        {
+        //    try
+          //  {
+                socketz.Bind(new IPEndPoint(myip, 0));
+                socketz.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+                byte[] bytrue = new byte[] { 1, 0, 0, 0 };
+                byte[] byout = new byte[] { 1, 0, 0, 0 };
+                socketz.IOControl(IOControlCode.ReceiveAll, bytrue, byout);
+                socketz.Blocking = false;
+                Array.Resize(ref bytedata, socketz.ReceiveBufferSize);
+                socketz.BeginReceive(bytedata, 0, bytedata.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+                comboBox1.Enabled = false;
+
+          //  }
+         //   catch (Exception ex)
+         //   {
+          //      comboBox1.BackColor = Color.Red;
+         //   }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+         
+
+            for (int i = 0; i <= mycomputerconnections[comboBox1.SelectedIndex].GetIPProperties().UnicastAddresses.Count - 1;i++)
+            {
+                AddressFamily adresdeneme, adreskontrol;
+                adreskontrol = AddressFamily.InterNetwork;
+                adresdeneme = mycomputerconnections[comboBox1.SelectedIndex].GetIPProperties().UnicastAddresses[i].Address.AddressFamily;
+
+                if (adresdeneme == adreskontrol)
+                {
+                    myip = mycomputerconnections[comboBox1.SelectedIndex].GetIPProperties().UnicastAddresses[i].Address;
+                    BindSocket();
+                }
+             
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (started == true)
+            {
+                button1.Text = "Başla";
+                started = false;
+            }
+            else
+            {
+                button1.Text = "Dur";
+                started = true;
+            }
+            
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+     
+            try
+            {
+                if (textBox1.Text != null)
+                {
+                    FilterIPAddress = IPAddress.Parse(textBox1.Text);
+                     FilterIP = true;
+                    textBox1.BackColor = Color.LimeGreen;
+                }
+                else
+                {
+                    FilterIP = false;
+                    textBox1.BackColor = Color.White;
+                }
+            }
+            catch (Exception exa)
+            {
+                FilterIP = false;
+                textBox1.BackColor = Color.White;
+            }
+
+        }
+    }
+}
